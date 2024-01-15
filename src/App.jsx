cf0a9f933d06ad47ee5a22f2d89515ca37f00281
solo-mizan <mizanbd.eee@gmail.com>
2024-01-15 11:11:45 +0600

    squre and board components are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-export function Square() {
+
+function Square({value}) {
   return (
-    <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
-      X
-    </button>
+    <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">{value}</button>
   );
 }
 
@@ -10,37 +9,19 @@ export default function Board() {
   return (
     <>
       <div>
-        <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
       </div>
       <div>
-        <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
       </div>
       <div>
-        <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
-        <button className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">
-          X
-        </button>
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
       </div>
     </>
   );
